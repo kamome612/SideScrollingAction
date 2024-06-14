c8302c434236c03d863332dc777fdf21a0a31eef
琉夏 吉田 <230395@jc-21.jp>
2024-06-14 11:36:15 +0900

    ちょい変更

--- Stage.cpp
+++ Stage.cpp
@@ -24,7 +24,7 @@ void Stage::Draw()
 	for (int h = 0; h < MAP_HEIGHT; h++) {
 		for (int w = 0; w < MAP_WIDTH; w++) {
 			int chip = MapData[w][h];
-			DrawRectGraph(w * 32, h * 32, 32 * (chip & 16), 32 * (chip / 16), 32, 32, hImage_, TURE, FALSE)
+			DrawRectGraph(w * 32, h * 32, 32 * (chip & 16), 32 * (chip / 16), 32, 32, hImage_, TURE, FALSE);
 		}
 	}
 }
