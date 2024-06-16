548c1a079bc73eddb5eb0a019b34a8efbe9fb2f7
琉夏 吉田 <230395@jc-21.jp>
2024-06-16 13:48:05 +0900

    書き直したりしたけど、直らない

--- Stage.cpp
+++ Stage.cpp
@@ -1,7 +1,8 @@
 #include "Stage.h"
+#include "Engine/CsvReader.h"
 
 Stage::Stage(GameObject* parent)
-	:GameObject(parent,"Stage"),hImage_(-1)
+	:GameObject(parent,"Stage"),hImage_(-1),width_(0),height_(0)
 {
 }
 
@@ -11,20 +12,49 @@ Stage::~Stage()
 
 void Stage::Initialize()
 {
-	hImage_ = LoadGraph("Assets/bgchar.png");
+	hImage_ = LoadGraph("Assets/Image/bgchar.png");
 	assert(hImage_ > 0);
+	map_ = nullptr;
 }
 
 void Stage::Update()
 {
+	if (map_ != nullptr) {
+		delete[] map_;
+		map_ = nullptr;
+	}
+
+	CsvReader csv;
+	bool ret = csv.Load("Assets/Image/stage1.csv");
+	assert(ret);
+	width_ = csv.GetWidth();
+	height_ = csv.GetHeight();
+	map_ = new int[height_ * width_];
+
+	for (int h = 0; h < height_; h++) {
+		if (csv.GetString(0, h) == "") {
+			height_ = h;
+			break;
+		}
+		for (int w = 0; w < width_; w++) {
+			map_[h * width_ + w] = csv.GetValue(w, h);
+		}
+	}
 }
 
 void Stage::Draw()
 {
-	for (int h = 0; h < MAP_HEIGHT; h++) {
+	/*for (int h = 0; h < MAP_HEIGHT; h++) {
 		for (int w = 0; w < MAP_WIDTH; w++) {
-			int chip = MapData[w][h];
-			DrawRectGraph(w * 32, h * 32, 32 * (chip & 16), 32 * (chip / 16), 32, 32, hImage_, TURE, FALSE);
+			int chip = MapData[h][w];
+			DrawRectGraph(w * 32, h * 32, 32 * (chip % 16), 32 * (chip / 16), 32, 32, hImage_, TRUE, FALSE);
+		}
+	}*/
+
+	for (int h = 0; h < height_; h++) {
+		for (int w = 0; w < width_; w++) {
+			int chip = map_[h * width_ + w];
+			DrawRectGraph(w * 32, h * 32, 32 * (chip % 16), 32 * (chip / 16), 32, 32, hImage_, TRUE, FALSE);
 		}
 	}
 }
