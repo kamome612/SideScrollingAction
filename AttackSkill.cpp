ebb3090e5c5b662b5d320911124f0e442c256acb
琉夏 吉田 <230395@jc-21.jp>
2024-10-10 16:31:24 +0900

    ミサイルがブロックを貫通しないようにした。

--- AttackSkill.cpp
+++ AttackSkill.cpp
@@ -4,6 +4,8 @@
 #include "Explosion.h"
 #include "Camera.h"
 #include "Enemy.h"
+#include "Stage.h"
+
 namespace {
 	const float SPEED_ = 600;
 	const float LimitTime_ = 1.5f;
@@ -35,9 +37,9 @@ void AttackSkill::Update()
 	{
 		KillMe();
 	}
-	if (transform_.position_.y < -30) {//画面外に出ていれば消す
-		KillMe();
-	}
+	//if (transform_.position_.y < -30) {//画面外に出ていれば消す
+	//	KillMe();
+	//}
 	//transform_.position_.x += SPEED_ * Time::DeltaTime();
 	//transform_.position_.y -= SPEED_ * Time::DeltaTime();
 	transform_.position_.x += cos(angle_) * SPEED_ * Time::DeltaTime();
@@ -61,6 +63,12 @@ void AttackSkill::Update()
 			pEx->SetPosition(transform_.position_.x - 32.0f, transform_.position_.y-64.0f);
 			KillMe();
 		}
+	}
+
+	Stage* pStage = GetParent()->FindGameObject<Stage>();
+	bool isHit = pStage->CollisionLeft(transform_.position_.x, transform_.position_.y + CHIP_SIZE / 1.5);
+	if (isHit) {
+		KillMe();
 	}
 }
 
